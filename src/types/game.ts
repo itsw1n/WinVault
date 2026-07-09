37469f72a4bc002c76d5bf411f50c8561d49d988
Win <erwincurato@gmail.com>
2026-07-10 00:31:36 +0800

    feat(types):add game and favoritegame type definitions

---
+++ src/types/game.ts
@@ -0,0 +1,18 @@
+export interface Game {
+  id: string
+  title: string
+  thumbnailUrl: string
+  shortDescription: string
+  externalUrl: string
+  genre: string
+  tags: string[]
+  owner: { username: string }
+  _count: { favorites: number }
+  createdAt?: Date
+  updatedAt?: Date
+}
+
+export interface FavoriteGame {
+  game: Game
+  createdAt: Date
+}
