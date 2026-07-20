44c6d23b2e30492af69d1b52ec7a6bfbdd91a35a
Win <erwincurato@gmail.com>
2026-07-20 23:31:39 +0800

    refactor(ui):remove page-client.tsx pattern, move components, add protected boundaries

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,7 +1,7 @@
 import { Navbar } from "@/components/layout/navbar"
 import { Footer } from "@/components/layout/footer"
 
-export default function DashboardLayout({
+export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode
